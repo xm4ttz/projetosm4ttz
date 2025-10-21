4d43a8f91f0c0286125ae1755a7785edb51d6f3e
xm4ttz <matthew.coelho1406@gmail.com>
2025-10-21 15:51:14 +0000

    teste1

--- Batalha Naval/1 - Novato/teste1.c
+++ Batalha Naval/1 - Novato/teste1.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
  
 int main() {
-    char *nomes[] = {"Alice", "Bob", "Carol"};
-    int i;
-    for(i = 0; i < 3; i++) {
-        printf("%s\n", nomes[i]);
-    }
+    int numeros[5];
+ 
+    printf("Digite o primeiro número: \n");
+    scanf("%d", &numeros[0]);
+
+    printf("Digite o segundo número: \n");
+    scanf("%d", &numeros[1]);
+    
+    printf("Digite o terceiro número: \n");
+    scanf("%d", &numeros[2]);
+    
+    printf("Digite o quarto número: \n");
+    scanf("%d", &numeros[3]);
+    
+    printf("Digite o quinto número: \n");
+    scanf("%d", &numeros[4]);
+
+    printf("Segue os números na ordem do vetor: %d, %d, %d, %d, %d", numeros[0], numeros[1], numeros[2], numeros[3], numeros[4]);
+    
     return 0;
 }
